78457205029671993f1bdb9d92898b7c40af5d98
Mandar Chandorkar <mandar2812@gmail.com>
2021-05-14 21:31:16 +0200

    Mods to sampler.

--- run_sampler.py
+++ run_sampler.py
@@ -8,17 +8,9 @@ from pathlib import Path
 import datetime
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from cycler import cycler
-import prettyprinter
-from prettyprinter import cpprint
 from ray import tune
-# from ray.tune.suggest import ConcurrencyLimiter
-# from ray.tune.schedulers import AsyncHyperBandScheduler
-# from ray.tune.suggest.dragonfly import DragonflySearch
 import models
 import data_utils
-prettyprinter.install_extras(exclude=["ipython", "django"])
 
 
 def load_data(state: str, start_date: datetime.datetime) -> pd.DataFrame:
@@ -31,6 +23,7 @@ def tuning_exp(
         state: str,
         population: int,
         start_date: datetime.datetime,
+        hyp_config: Dict[str, Any],
         num_samples: int = 50):
     """Perform the sampling experiment."""
     def _tuning_fn(config: Dict[str, Any]):
@@ -47,6 +40,7 @@ def tuning_exp(
         I0 = int(start_mh.Confirmed - (start_mh.Cured + start_mh.Deaths))
         R0 = (start_mh.Cured)
         D0 = start_mh.Deaths
+
         simulator.integrate(
             timesteps=data.shape[0]-1,
             t_min=1,
@@ -59,50 +53,53 @@ def tuning_exp(
         )
 
         projected_results = pd.DataFrame({
-            "Confirmed": (simulator.I.cumsum() + start_mh.Confirmed),
-            "Cured": simulator.R,
-            "Deaths": simulator.D
+            "rest": population - (simulator.I + simulator.D),
+            "infected": simulator.I,
+            "dead": simulator.D
         })
 
         y_pred = projected_results.to_numpy() / population
-        y_pred_aug = np.concatenate(
-            [
-                1.0 - np.sum(y_pred, axis=-1, keepdims=True),
-                y_pred
-            ],
-            axis=-1
-        )
-        y_actual = data.loc[data.Date > start_date, ["Confirmed", "Cured", "Deaths"]].to_numpy() / population
-        y_actual_aug = np.concatenate(
-            [
-                1.0 - np.sum(y_actual, axis=-1, keepdims=True),
-                y_actual
-            ],
-            axis=-1
+        # y_pred_aug = np.concatenate(
+        #     [
+        #         1.0 - np.sum(y_pred, axis=-1, keepdims=True),
+        #         y_pred
+        #     ],
+        #     axis=-1
+        # )
+
+        case_data: pd.DataFrame = data.loc[
+            data.Date > start_date,
+            ["Confirmed", "Cured", "Deaths"]
+        ]
+        projected_case_data = pd.DataFrame(
+            {
+                "rest": population - (case_data["Confirmed"] - case_data["Cured"] + case_data["Deaths"]),
+                "infected": case_data["Confirmed"] - case_data["Cured"],
+                "dead": case_data["Deaths"]
+            }
         )
 
-        tune.report(kl_div=np.mean(np.sum(y_pred_aug*np.log(y_pred_aug/y_actual_aug), axis=-1)))
+        y_actual = projected_case_data.to_numpy() / population
+        # y_actual_aug = np.concatenate(
+        #     [
+        #         1.0 - np.sum(y_actual, axis=-1, keepdims=True),
+        #         y_actual
+        #     ],
+        #     axis=-1
+        # )
+
+        tune.report(
+            kl_div=np.mean(np.sum(y_pred*np.log(y_pred/y_actual), axis=-1))
+        )
 
     analysis = tune.run(
         _tuning_fn,
-        config={
-            "beta": tune.uniform(0.4, 1.2),
-            "eps": tune.uniform(1. / 15., 1. / 3.),
-            "mu": tune.loguniform(0.001, 0.05),
-            "rho": tune.loguniform(1e-4, 1e-3),
-            "p_d": tune.loguniform(0.005, 0.05),
-        },
+        config=hyp_config,
         metric="kl_div",
         mode="min",
         num_samples=num_samples,
-        local_dir=Path.cwd() / ".tuning"
-        # search_alg=ConcurrencyLimiter(
-        #     DragonflySearch(
-        #         optimizer="bandit",
-        #         domain="euclidean"
-        #     ),
-        #     max_concurrent=12
-        # ),
+        local_dir=Path.cwd() / ".tuning",
+        verbose=2
     )
 
     print(
